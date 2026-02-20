8fc15eb144025bf4d1381f898bf928f260bf5b1a
afatiyev17271 <afatiyev17271@ada.edu.az>
2026-02-20 10:54:36 +0400

    Lab03

--- lab03.java
+++ lab03.java
@@ -2,79 +2,26 @@ import java.util.Scanner;
 
 public class lab03 {
 
-    public static int findMin(int[] arr) {
-        int min = arr[0];
+    public static String reverseString(String text) {
 
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i] < min) {
-                min = arr[i];
-            }
-        }
-        return min;
-    }
+        StringBuilder reversed = new StringBuilder();
 
-    public static int findMax(int[] arr) {
-        int max = arr[0];
-
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i] > max) {
-                max = arr[i];
-            }
-        }
-        return max;
-    }
-
-    public static int[] findMinMax(int[] arr) {
-        int min = arr[0];
-        int max = arr[0];
-
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i] < min) {
-                min = arr[i];
-            }
-            if (arr[i] > max) {
-                max = arr[i];
-            }
+        for (int i = text.length() - 1; i >= 0; i--) {
+            reversed.append(text.charAt(i));
         }
 
-        int[] result = {min, max};
-        return result;
+        return reversed.toString();
     }
 
     public static void main(String[] args) {
 
-        int[] cmdArray = new int[args.length];
-
-        for (int i = 0; i < args.length; i++) {
-            cmdArray[i] = Integer.parseInt(args[i]);
-        }
-
-        if (cmdArray.length > 0) {
-            System.out.println("From command line:");
-            System.out.println("Minimum: " + findMin(cmdArray));
-            System.out.println("Maximum: " + findMax(cmdArray));
-
-            int[] both = findMinMax(cmdArray);
-            System.out.println("Both together -> Min: " + both[0] + " Max: " + both[1]);
-        }
-
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter size of array: ");
-        int n = sc.nextInt();
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
 
-        int[] arr = new int[n];
+        String result = reverseString(input);
 
-        System.out.println("Enter " + n + " numbers:");
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-        }
-
-        System.out.println("From scanner:");
-        System.out.println("Minimum: " + findMin(arr));
-        System.out.println("Maximum: " + findMax(arr));
-
-        int[] both2 = findMinMax(arr);
-        System.out.println("Both together -> Min: " + both2[0] + " Max: " + both2[1]);
+        System.out.println("Reversed string: " + result);
     }
 }
